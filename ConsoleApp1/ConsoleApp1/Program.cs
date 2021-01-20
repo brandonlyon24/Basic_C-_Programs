43b14f52eaf1d51b8f693e63db475dc2dd4fe1e1
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2021-01-20 13:06:48 -0800

    Learning C#

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@
         static void Main()
         {
             Console.WriteLine("Welcome to Acme Accounting Systems \n Remember, we're \"accounting\" on you!");
-        Console.ReadLine();
+            Console.ReadLine();
         }
     }
 

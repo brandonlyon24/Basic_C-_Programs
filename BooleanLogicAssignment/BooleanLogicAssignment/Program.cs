5bc98d3611e6ab91469c3f6015a289323cbe98fd
brandonlyon24 <72480983+brandonlyon24@users.noreply.github.com>
2021-01-23 13:23:45 -0800

    Commit

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can we approve you for insurance?");
+            Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("True or False. Have you ever had a dui?");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            bool qualified = (age >= 15 || dui == false || tickets <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
